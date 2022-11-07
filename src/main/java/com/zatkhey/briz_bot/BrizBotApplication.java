bb3a9a17a409f22b924431db66af74c40207b1fa
ZatkheY <1Lsdsimon>
2022-11-07 15:57:43 +0200

    new commit

--- src/main/java/com/zatkhey/briz_bot/BrizBotApplication.java
+++ src/main/java/com/zatkhey/briz_bot/BrizBotApplication.java
@@ -2,8 +2,10 @@ package com.zatkhey.briz_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BrizBotApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/zatkhey/briz_bot/sevice/PingSevice.java
@@ -0,0 +1,35 @@
+package com.zatkhey.briz_bot.sevice;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Service
+@Getter
+@Setter
+@Slf4j
+public class PingSevice {
+    @Value("${pingtask.url}")
+    private String url;
+
+    @Scheduled(fixedRateString = "${pingtask.period}")
+    public void pingMe(){
+        try {
+            URL url = new URL(getUrl());
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            log.info("Ping {}, OK: response code {}", url.getHost(), connection.getResponseCode());
+            connection.disconnect();
+        } catch (IOException e) {
+            log.error("Ping FAILED");
+            e.printStackTrace();
+        }
+    }
+}
