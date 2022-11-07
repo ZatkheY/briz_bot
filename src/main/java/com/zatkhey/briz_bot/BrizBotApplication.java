1255c319f5bc153ca05f6485ae7fab93a057ab21
ZatkheY <1Lsdsimon>
2022-11-07 19:50:41 +0200

    new commit

--- src/main/java/com/zatkhey/briz_bot/BrizBotApplication.java
+++ src/main/java/com/zatkhey/briz_bot/BrizBotApplication.java
@@ -2,7 +2,7 @@ package com.zatkhey.briz_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
 public class BrizBotApplication {
